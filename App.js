506c122fe42711e0e61b6d593eea6245ce06c73e
Jezreel Julao <jezreel.julao@yahoo.com>
2019-08-18 22:45:46 -0700

    Play testing styles

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Project Start</Text>
+        <Text style={styles.text}>Project Start</Text>
+        <Text style={[styles.bigFont, styles.text]}>Big Font</Text>
       </View>
     );
   }
@@ -14,10 +15,15 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    color: 'red',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#ddbaba',
+  },
+  bigFont: {
+    fontSize: 30,
   },
 });
 
