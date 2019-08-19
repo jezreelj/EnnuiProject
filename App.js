d6e6b7cdff9190b309e9f065149251fef524a36f
Jezreel Julao <jezreel.julao@yahoo.com>
2019-08-18 22:16:55 -0700

    Rewrite App.js as a class extending component

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Project Start</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Project Start</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    color: 'red',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+AppRegistry.registerComponent('Ennui', () => App);
