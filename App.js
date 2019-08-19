f118bd00ae1c27b0699de6116379e3bdbfb698eb
Jezreel Julao <jezreel.julao@yahoo.com>
2019-08-18 21:03:38 -0700

    Testing branch and PR

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Nathan Woo</Text>
+      <Text>Project Start</Text>
     </View>
   );
 }
