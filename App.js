e128f11fd52df4c01cc1a42e56cf0dd2629a7134
Jezreel Julao <jezreel.julao@yahoo.com>
2019-08-22 11:50:01 -0700

    eslint Styling guide

--- App.js
+++ App.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native'
-import { Button } from 'react-native-elements'
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default class App extends Component {
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Project Start</Text>
-        <Text style={[styles.bigFont, styles.text]}>Big Font</Text>
-        <Button title="Solid Button"
-          onPress={() => {
-            alert('You tapped the button!')
-          }}
-        />
-      </View>
-    )
-  }
+	render () {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.text}>Project Start</Text>
+				<Text style={[styles.bigFont, styles.text]}>Big Font</Text>
+				<Button title="Solid Button"
+					onPress={() => {
+						alert('You tapped the button!');
+					}}
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#ddbaba'
-  },
-  bigFont: {
-    fontSize: 30
-  }
-})
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	text: {
+		color: '#ddbaba'
+	},
+	bigFont: {
+		fontSize: 30
+	}
+});
 
-AppRegistry.registerComponent('Ennui', () => App)
+AppRegistry.registerComponent('Ennui', () => App);
